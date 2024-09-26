330e359f86d9a9d91b460d550fdb58389d7ca871
Stanislav Pidhorskyi <stpidhorskyi@fb.com>
2024-09-25 23:50:46 -0400

    Updated license

--- drtk/msi_ext.pyi
+++ drtk/msi_ext.pyi
@@ -1,17 +1,16 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
 #
-# This source code is licensed under the license found in the
+# This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
 from torch import Tensor
 
 def msi(
-    ray_o: th.Tensor,
-    ray_d: th.Tensor,
-    texture: th.Tensor,
+    ray_o: Tensor,
+    ray_d: Tensor,
+    texture: Tensor,
     sub_step_count: int,
     min_inv_r: float,
     max_inv_r: float,
     stop_thresh: float,
-) -> th.Tensor: ...
+) -> Tensor: ...
